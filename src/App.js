a23d16f7f8cd39bc08b0d012c5eacc248686b9cf
jbrea22 <josephbrea22@gmail.com>
2023-05-20 13:15:34 -0700

    new link

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   const [data,setData]=useState([]);
   const getData=()=>{
     var responseClone;
-    fetch('FSAE_2023/testing.json',{}
+    fetch('https://jbrea22.github.io/FSAE_2023/testing.json',{}
     )
       .then(function(response){
         responseClone=response.clone();
